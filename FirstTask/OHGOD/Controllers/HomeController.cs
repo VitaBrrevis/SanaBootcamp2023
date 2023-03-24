fe00434e48cc4a9eaf0d5bae4ae0327999fb4364
VitaBrrevis <vitalina.doronina@gmail.com>
2023-03-25 01:56:45 +0200

    i hope it works-

---
+++ FirstTask/OHGOD/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OHGOD.Models;
+using System.Diagnostics;
+
+namespace OHGOD.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public JsonResult Greeting(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                return Json(new { name = "There is no one to say hi to(" });
+            }
+
+            var greeting = "Hello, " + name + "!";
+            return Json(new { name = greeting });
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
